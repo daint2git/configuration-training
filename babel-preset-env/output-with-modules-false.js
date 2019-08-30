a731c3181d7b88289de5f7be0400c9c1e89ba3f9
daint2git <daint2dev@gmail.com>
2019-08-31 00:33:19 +0700

    babel-preset-env example

---
+++ babel-preset-env/output-with-modules-false.js
@@ -0,0 +1,128 @@
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+import React from 'react';
+import Button from './input2';
+import Text from './input3';
+export var handlePromise = function handlePromise() {
+  return new Promise(function (resolve, reject) {
+    resolve('hi');
+  });
+};
+export var handleAsyncAwait =
+/*#__PURE__*/
+function () {
+  var _ref = _asyncToGenerator(
+  /*#__PURE__*/
+  regeneratorRuntime.mark(function _callee() {
+    var greeting;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.next = 2;
+            return Promise.resolve('hi');
+
+          case 2:
+            greeting = _context.sent;
+            return _context.abrupt("return", greeting);
+
+          case 4:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  }));
+
+  return function handleAsyncAwait() {
+    return _ref.apply(this, arguments);
+  };
+}();
+export var myWeekMap = new WeakMap();
+export var mySet = new Set();
+export var myArray = Array.from([1, 2, 3], function (x) {
+  return x + x;
+});
+export var isExistedInArray = ['a', 'b'].includes('a');
+export var isExistedInString = 'ab'.includes('a');
+
+var Counter =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(Counter, _React$Component);
+
+  function Counter() {
+    var _getPrototypeOf2;
+
+    var _this;
+
+    _classCallCheck(this, Counter);
+
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(Counter)).call.apply(_getPrototypeOf2, [this].concat(args)));
+
+    _defineProperty(_assertThisInitialized(_this), "state", {
+      count: 0
+    });
+
+    _defineProperty(_assertThisInitialized(_this), "handleIncrement", function () {
+      _this.setState(function (_ref2) {
+        var count = _ref2.count;
+        return {
+          count: count + 1
+        };
+      });
+    });
+
+    _defineProperty(_assertThisInitialized(_this), "handleDecrement", function () {
+      _this.setState(function (_ref3) {
+        var count = _ref3.count;
+        return {
+          count: count - 1
+        };
+      });
+    });
+
+    return _this;
+  }
+
+  _createClass(Counter, [{
+    key: "render",
+    value: function render() {
+      return React.createElement("div", null, React.createElement(Button, {
+        onClick: this.handleIncrement
+      }, "+"), React.createElement(Button, {
+        onClick: this.handleDecrement
+      }, "-"), React.createElement(Text, null, this.state.count));
+    }
+  }]);
+
+  return Counter;
+}(React.Component);
+
+export default Counter;
